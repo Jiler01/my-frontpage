d74f8fd9363bb9a3dd178c370a2e29405e34a7a3
Jiler01 <jl10141516@gmail.com>
2023-09-05 22:18:08 +0200

    adding some kind of an api ig ?

---
+++ app/api/route.tsx
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+export function GET() {
+  return NextResponse.json(DATA);
+}
+
+const DATA = {
+  Life: {
+    title: <>Fisrt article !!</>,
+    content: (
+      <>
+        The more I live, the more I learn. The more I learn, the more I realize,
+        the less I know.
+      </>
+    ),
+  },
+  SiO2: {
+    title: <>Second article !!</>,
+    content: (
+      <>
+        The more I live, the more I learn. The more I learn, the more I realize,
+        the less I know.
+      </>
+    ),
+  },
+};
