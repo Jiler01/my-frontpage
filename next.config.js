5c9d7c968621bd46e1f18489f51ffbf223472bcf
Jiler01 <jl10141516@gmail.com>
2023-09-11 09:44:20 +0200

    trying another building setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const repo = "my-frontpage";
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-module.exports = nextConfig
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: "imgix",
+    path: 'the "domain" of your Imigix source',
+  },
+};
+
+module.exports = nextConfig;
